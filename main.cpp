25c13f1752c98cde0cf63a16596935bca9f8eb98
Carl Woffenden <cwoffenden@gmail.com>
2026-06-18 18:26:54 +0200

    Initial test of extracting texture data

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cstdlib>
 
 #if TARGET_OS_MAC && !TARGET_OS_IPHONE
@@ -12,6 +13,56 @@
 #endif
 
 #include <GLFW/glfw3.h>
+
+struct BCBlock {
+	union {
+		uint8_t bytes[8];
+		struct {
+			union {
+				uint16_t rgb565;
+				struct {
+					uint16_t b: 5;
+					uint16_t g: 6;
+					uint16_t r: 5;
+				};
+			} endpt[2];
+			union {
+				uint32_t bits;
+				struct {
+					uint8_t x0: 2;
+					uint8_t x1: 2;
+					uint8_t x2: 2;
+					uint8_t x3: 2;
+				} y[4];
+			} texels;
+		} bc1;
+		uint64_t data;
+	};
+};
+
+static_assert(sizeof(BCBlock) == 8, "BC block should be 8 bytes");
+
+BCBlock block[2] = {};
+
+GLuint txName = 0;
+
+void setup() {
+	block[1].bc1.endpt[0].r = 31;
+	block[1].bc1.endpt[1].r = 9;
+	block[1].bc1.texels.bits = 0xBB44BB44;
+	glGenTextures(1, &txName);
+	glBindTexture(GL_TEXTURE_2D, txName);
+	glCompressedTexImage2D(GL_TEXTURE_2D, 0, GL_COMPRESSED_RGBA_S3TC_DXT3_EXT, 4, 4, 0, 16, &block);
+
+	float f32[4][4];
+	glGetTexImage(GL_TEXTURE_2D, 0, GL_RED, GL_FLOAT, f32);
+	uint16_t u16[4][4];
+	glGetTexImage(GL_TEXTURE_2D, 0, GL_RED, GL_UNSIGNED_SHORT, u16);
+	uint8_t u08[4][4];
+	glGetTexImage(GL_TEXTURE_2D, 0, GL_RED, GL_UNSIGNED_BYTE, u08);
+	GLenum err = glGetError();
+	assert(err == 0);
+}
 
 void draw(GLFWwindow* window) {
 	int fbW, fbH;
@@ -37,6 +88,8 @@ int main(int /*argc*/, char* /*argv*/[]) {
 		exit(EXIT_FAILURE);
 	}
 	glfwMakeContextCurrent(window);
+
+	setup();
 
 	while (!glfwWindowShouldClose(window)) {
 		draw(window);
